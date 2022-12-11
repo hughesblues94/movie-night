102a11295f88d149a19a8837a1afc763909bfa03
hughesblues94 <hughesblues94@gmail.com>
2022-12-11 18:44:23 +0000

    added a get started button that displays the GenreLIst component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { useState } from 'react';
 import './App.css';
 import GenreList from './Components/GenreList';
 
 function App() {
+
+  const [start, setStart] = useState(false)
+
+
   return (
     <div className="App">
       <h1>Movie Night!</h1>
-      <GenreList />
+
+      <button onClick={() => setStart(true)}>Get Started</button>
+      {start && <GenreList />}
     </div>
   );
 }
