5efab51ec80d551d31de10ae052af77ee8687db7
hughesblues94 <hughesblues94@gmail.com>
2022-12-15 12:58:09 +0000

    added functionality that fetches movie posters and ratings.

--- src/Components/MovieSuggestion.js
+++ src/Components/MovieSuggestion.js
@@ -12,15 +12,33 @@ const MovieSuggestion = ({ results }) => {
 
         axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=15e383204c1b8a09dbfaaa4c01ed7e17&&with_genres=${genreId}`)
             .then((res) => setMovies(res.data.results))
+
             .catch((err) => console.log(err))
     }, [results.genre]);
 
 
     console.log("here are the movies ==> ", movies)
 
+    const moviePosters = movies.map((movie) => movie.poster_path)
+    console.log("this is movie posters at position 0 => ", moviePosters[0])
+    const firstMovie = moviePosters[0]
+
+
+    //this is the array of movie ratings
+    const movieRatings = movies.map((movie) => movie.vote_average)
+    console.log("these are the movie ratings ===>", movieRatings)
+    movieRatings.sort(function (a, b) { return b - a });
+    const highestValue = movieRatings[0]
+    console.log(highestValue)
+
+
+
+    const firstMovieAverage = movieRatings[0]
+
     return (
         <div>
-
+            {movies && <img alt="poster-img" src={`https://image.tmdb.org/t/p/w185${firstMovie}`}></img>}
+            {movies && <h1>{firstMovieAverage}</h1>}
         </div>
     );
 }
