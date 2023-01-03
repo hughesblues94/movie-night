4804991e223aea9e64c8da62aef7f0201e530d8b
hughesblues94 <hughesblues94@gmail.com>
2023-01-03 16:16:02 +0000

    fix movie suggestion

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -5,9 +5,9 @@ const MovieCard = ({ movie }) => {
     return (
         <div className='container'>
             <h2> Your movie for tonight: </h2>
-            <h1 className='movie-title'>{movie.original_title}</h1>
-            <img alt="movie poster" src={`https://image.tmdb.org/t/p/w185${movie.poster_path}`} />
-            <p className='desc'> {movie.overview} </p>
+            <h1 className='movie-title'>{movie?.original_title}</h1>
+            <img alt="movie poster" src={`https://image.tmdb.org/t/p/w185${movie?.poster_path}`} />
+            <p className='desc'> {movie?.overview} </p>
 
         </div>
     );
