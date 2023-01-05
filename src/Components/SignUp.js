43f8bc31deb27a368c8ece7b3ea9babbaf10effa
hughesblues94 <hughesblues94@gmail.com>
2023-01-05 15:24:28 +0000

    add a confirm password field that checks for minimum 6 characters, letter and a number

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,8 +1,13 @@
 import { Form, redirect } from "react-router-dom";
+import { useState } from "react";
+import '../Styles/SignUp.css'
 
 export default function SignUp() {
+
+    const [errors, setErrors] = useState("")
+
     return (
-        <div>
+        <div className="form">
             <Form method="post" action="" >
                 <label>
                     <span>Your Name:</span>
@@ -13,8 +18,14 @@ export default function SignUp() {
                     <input type="email" name="email" required></input>
                 </label>
                 <label>
-                    <span>Password:</span>
-                    <input type="password" name="password" required></input>
+                    <span>Password (minimum 6 characters, must contain a letter and a number)</span>
+                    <input type="password" name="password" required pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{5,}$"
+                    ></input>
+                </label>
+                <label>
+                    <span>Confirm Password:</span>
+                    <input type="password" name="confirmPassword" required pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{5,}$"
+                    ></input>
                 </label>
                 <button>Submit</button>
             </Form>
@@ -28,10 +39,14 @@ export const userSignUp = async ({ request }) => {
     const submission = {
         username: data.get('name'),
         email: data.get('email'),
-        password: data.get('password')
+        password: data.get('password'),
+        confirmPassword: data.get('confirmPassword')
     }
     console.log(submission)
 
+    if (submission.password !== submission.confirmPassword) {
+        console.log("Passwords did not match, please try again")
+    }
 
 
     // send post request
